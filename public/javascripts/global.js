4dd6fb6387828a0add94fc0618a0437482f1333a
Sergio <you@example.com>
2018-03-22 22:49:54 +0100

    añadir delete

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -8,6 +8,8 @@ $(document).ready(function () {
   $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
   // Add User button click
   $('#btnAddUser').on('click', addUser);
+  // Delete User link click
+  $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 });
 // Functions =============================================================
 
@@ -98,6 +100,34 @@ function addUser (event) {
   } else {
     // If errorCount is more than 0, error out
     alert('Please fill in all fields');
+    return false;
+  }
+}
+
+// Delete User
+function deleteUser (event) {
+  event.preventDefault();
+
+  // Pop up a confirmation dialog
+  var confirmation = confirm('Are you sure you want to delete this user?');
+
+  // Check and make sure the user confirmed
+  if (confirmation === true) {
+    // If they did, do our delete
+    $.ajax({
+      type: 'DELETE',
+      url: '/users/deleteuser/' + $(this).attr('rel')
+    }).done(function (response) {
+      // Check for a successful (blank) response
+      if (response.msg === '') {
+      } else {
+        alert('Error: ' + response.msg);
+      }
+      // Update the table
+      populateTable();
+    });
+  } else {
+  // If they said no to the confirm, do nothing
     return false;
   }
 }
