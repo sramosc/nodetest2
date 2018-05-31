b2458c8c6a7df23fe9b1adc328c0896a8f24a108
Sergio Ramos Calvente <sergiorc@localhost.localdomain>
2018-05-31 17:07:02 +0200

    reset

--- routes/employees.js
+++ routes/employees.js
@@ -51,4 +51,175 @@ router.put('/updateEmployee/:code', function (req, res) {
   });
 });
 
+// GET resetCollectionEmployees
+router.get('/resetCollectionEmployees', function (req, res) {
+  var db = req.db;
+  var collection = db.get('employees');
+  collection.remove({});
+  collection.insert([
+    {
+      "code": "1",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT1"
+    },
+    {
+      "code": "2",
+      "name": "Pablo Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT2"
+    },
+    {
+      "code": "3",
+      "name": "Carlos Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT3"
+    },
+    {
+      "code": "4",
+      "name": "Jose Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT1"
+    },
+    {
+      "code": "5",
+      "name": "Rafael Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": ""
+    },
+    {
+      "code": "6",
+      "name": "Yolanda Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT4"
+    },
+    {
+      "code": "7",
+      "name": "Trinidad Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": ""
+    },
+    {
+      "code": "8",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT5"
+    },
+    {
+      "code": "9",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT5"
+    },
+    {
+      "code": "10",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT6"
+    },
+    {
+      "code": "11",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": ""
+    },
+    {
+      "code": "12",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT2"
+    },
+    {
+      "code": "13",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT2"
+    },
+    {
+      "code": "14",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT2"
+    },
+    {
+      "code": "15",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT5"
+    },
+    {
+      "code": "16",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT4"
+    },
+    {
+      "code": "17",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT6"
+    },
+    {
+      "code": "18",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT6"
+    },
+    {
+      "code": "19",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT1"
+    },
+    {
+      "code": "20",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT1"
+    },
+    {
+      "code": "21",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT3"
+    },
+    {
+      "code": "22",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT3"
+    },
+    {
+      "code": "23",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT3"
+    },
+    {
+      "code": "24",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT2"
+    },
+    {
+      "code": "25",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT2"
+    },
+    {
+      "code": "26",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT5"
+    },
+    {
+      "code": "27",
+      "name": "Ana Pérez",
+      "mail": "anaperez@entelgy.com",
+      "ounit": "UNIT5"
+    }
+  ]);  
+});
+
 module.exports = router;
