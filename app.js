557c724334d84322d59b7f412945784e099618b9
Sergio Ramos Calvente <sergiorc@localhost.localdomain>
2018-06-04 11:18:51 +0200

    servicio calendar

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var employeesRoutes = require('./routes/employees');
 var oUnitsRoutes = require('./routes/ounits');
+var calendarRoutes = require('./routes/calendar');
 
 
 var app = express();
@@ -39,6 +40,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/employees', employeesRoutes);
 app.use('/ounits', oUnitsRoutes);
+app.use('/calendars', calendarRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/calendars.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+
+// GET ounits list
+router.get('/listCalendars', function (req, res) {
+  var db = req.db;
+  var collection = db.get('calendars');
+  collection.find({}, {}, function (e, docs) {
+    res.json(docs);
+  });
+});
+
+// GET employee
+router.get('/getCalendar/:id', function (req, res) {
+  var db = req.db;
+  var collection = db.get('calendars');
+  var docToFind = req.params.id;
+  collection.find({ 'id': docToFind }, {}, function (e, docs) {
+    res.json(docs);
+  });
+});
+
+// POST addEmployee.
+router.post('/addCalendar', function (req, res) {
+  var db = req.db;
+  var collection = db.get('calendars');
+  collection.insert(req.body, function (err, result) {
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
+});
+
+// DELETE delEmployee
+router.delete('/delCalendar/:id', function (req, res) {
+  var db = req.db;
+  var collection = db.get('calendars');
+  var docToDelete = req.params.id;
+  collection.remove({ 'id': docToDelete }, function (err) {
+    res.send((err === null) ? { msg: '' } : { msg: 'error: ' + err });
+  });
+});
+
+// PUT updateEmployee
+router.put('/updateCalendar/:id', function (req, res) {
+  var db = req.db;
+  var collection = db.get('calendars');
+  var docToUpdate = req.params.id;
+  collection.update({ 'id': docToUpdate }, req.body, function (err) {
+    res.send((err === null) ? { msg: '' } : { msg: 'error: ' + err });
+  });
+});
+
+// GET resetCollectionEmployees
+router.get('/resetCollectionCalendars', function (req, res) {
+  var db = req.db;
+  var collection = db.get('calendars');
+  collection.remove({});
+  collection.insert([
+    {
+      'name': 'Nvidad',
+      'anio': '2018',
+      'tipo': 'Laboral',
+    },
+    {
+      'name': 'Semana Santa',
+      'anio': '2017',
+      'tipo': 'Festivo',
+    }
+  ]);
+});
+
+module.exports = router;
