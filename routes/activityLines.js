de7b2a23632ea64020b0b93da088e676240f5501
sergio.ramos <sergio.ramos@entelgydigital.com>
2018-09-12 10:08:29 +0200

    collation

--- routes/activityLines.js
+++ routes/activityLines.js
@@ -12,7 +12,7 @@ router.get('/listActivityLines', function (req, res) {
       }
     },
     { $sort: { id: 1 } }
-  ], {}, function (e, docs) {
+  ], {collation:{numericOrdering: true}}, function (e, docs) {
     if (e != null) {
       res.json(e)
     } else {
